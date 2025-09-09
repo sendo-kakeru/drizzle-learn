4e98baf10c027a4399da276a0be75939eb998048
sendo-kakeru <sendokakeru.js@gmail.com>
2025-09-09 21:38:30 +0900

    feat: add biome

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,31 +1,31 @@
-import { Hono } from "hono";
 import { drizzle } from "drizzle-orm/node-postgres";
-import { usersTable } from "./db/schema";
+import { Hono } from "hono";
 import { env } from "hono/adapter";
+import { usersTable } from "./db/schema";
 
 const app = new Hono();
 
 app.get("/", (c) => {
-  return c.text("Hello Hono!");
+	return c.text("Hello Hono!");
 });
 
 app.get("/users", async (c) => {
-  const { DATABASE_URL } = env<{ DATABASE_URL: string }>(c);
-  const db = drizzle(DATABASE_URL);
-  const users = await db.select().from(usersTable);
-  return c.json(users);
+	const { DATABASE_URL } = env<{ DATABASE_URL: string }>(c);
+	const db = drizzle(DATABASE_URL);
+	const users = await db.select().from(usersTable);
+	return c.json(users);
 });
 
 app.post("/users", async (c) => {
-  const { DATABASE_URL } = env<{ DATABASE_URL: string }>(c);
-  const db = drizzle(DATABASE_URL);
-  await db.insert(usersTable).values({
-    name: "John",
-    age: 30,
-    email: "john@example.com",
-  } satisfies typeof usersTable.$inferInsert);
+	const { DATABASE_URL } = env<{ DATABASE_URL: string }>(c);
+	const db = drizzle(DATABASE_URL);
+	await db.insert(usersTable).values({
+		name: "John",
+		age: 30,
+		email: "john@example.com",
+	} satisfies typeof usersTable.$inferInsert);
 
-  return c.json({ message: "ok" });
+	return c.json({ message: "ok" });
 });
 
 export default app;
